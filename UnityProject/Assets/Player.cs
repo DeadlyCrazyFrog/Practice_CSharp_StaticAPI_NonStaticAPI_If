3cf30f3aa02e53e5a5400d03f4fe6d943e07c924
DeadlyCrazyFrog <37039662+DeadlyCrazyFrog@users.noreply.github.com>
2020-12-31 17:15:59 +0800

    字體顏色

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    [Range(0f, 100f)]
+    public float HP = 1.5F;
+    [Header("攻擊力")]
+    [Range(0f, 50f)]
+    public float ATK = 30f;
+    [Header("玩家 Player")]
+    public GameObject target;
+    [Header("喇叭、攻擊音效")]
+    public AudioSource AUD;
+    public AudioClip Atk_sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color P1_color = Color.blue;
+        Color P2_color = Color.red;
+        Zombie zombie = target.GetComponent<Zombie>();
+        string message = "Player " + zombie.HP;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            Debug.Log(string.Format("<color=blue>{0}</color>", zombie.HP));
+            print(string.Format("<color=blue>{0}</color>", zombie.HP));
+        }
+    }
+}
---
+++ UnityProject/Assets/Zombie.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("血量")]
+    [Range(0f, 100f)]
+    public float HP = 1.5F;
+    [Header("攻擊力")]
+    [Range(0f, 50f)]
+    public float ATK = 30f;
+    [Header("玩家 Player")]
+    public GameObject target;       
+    [Header("喇叭、攻擊音效")]
+    public AudioSource AUD;    
+    public AudioClip Atk_sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player player = target.GetComponent<Player>();
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            print(player.HP);
+        }
+    }
+}
